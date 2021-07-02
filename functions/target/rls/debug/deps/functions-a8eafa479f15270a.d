e6f91ffa60e15c2691fcfa405b0df4139f773136
clay <clay@clbx.io>
2021-07-02 01:35:00 -0400

    Delete functions/target directory

--- functions/target/rls/debug/deps/functions-a8eafa479f15270a.d
+++
@@ -1,5 +0,0 @@
-/workspace/rust-book/functions/target/rls/debug/deps/functions-a8eafa479f15270a.rmeta: src/main.rs
-
-/workspace/rust-book/functions/target/rls/debug/deps/functions-a8eafa479f15270a.d: src/main.rs
-
-src/main.rs:
