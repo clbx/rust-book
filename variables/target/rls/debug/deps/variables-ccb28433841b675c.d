793019e346b2ba7435cc8e04dbacb09e05ff3ae1
clay <clay@clbx.io>
2021-07-02 01:35:25 -0400

    Delete variables/target directory

--- variables/target/rls/debug/deps/variables-ccb28433841b675c.d
+++
@@ -1,5 +0,0 @@
-/workspace/rust-book/variables/target/rls/debug/deps/variables-ccb28433841b675c.rmeta: src/main.rs
-
-/workspace/rust-book/variables/target/rls/debug/deps/variables-ccb28433841b675c.d: src/main.rs
-
-src/main.rs:
