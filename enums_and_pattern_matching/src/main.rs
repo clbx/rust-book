60d7183e1cb89ae3ac797828694908c8c836b22d
Clay Buxton <clay@clbx.io>
2021-07-07 04:53:12 +0000

    enum

--- enums_and_pattern_matching/src/main.rs
+++ enums_and_pattern_matching/src/main.rs
@@ -60,6 +60,34 @@ fn main() {
     let m = Message::Write(String::from("Hello"));
     m.call();
     
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky Penny!");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state)
+            },
+        }
+    }
+
+    
 
 
 }
